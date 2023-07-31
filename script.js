b37b6d73be2e3e7843312ddfe4c5d90aea68f4e5
paula <paulalaorga@gmail.com>
2023-07-31 13:23:01 +1000

    all

--- script.js
+++ script.js
@@ -1,29 +1,4 @@
-
-document.addEventListener("DOMContentLoaded", function () {
-  const openContactFormButton = document.getElementById("openContactForm");
-  const contactFormModal = document.getElementById("contactFormModal");
-  const closeContactFormButton = contactFormModal.querySelector(".close");
-
-  openContactFormButton.addEventListener("click", function () {
-    contactFormModal.style.display = "block";
+document.getElementById('openContactForm').addEventListener('click', function() {
+    var form = document.getElementById('contactForm');
+    form.classList.toggle('hidden');
   });
-
-  closeContactFormButton.addEventListener("click", function () {
-    contactFormModal.style.display = "none";
-  });
-
-  window.addEventListener("click", function (event) {
-    if (event.target === contactFormModal) {
-      contactFormModal.style.display = "none";
-    }
-  });
-
-  // You can add form submission handling here.
-  const contactForm = document.getElementById("contactForm");
-  contactForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    // Process the form data here or send it to a server using AJAX.
-    // You can also display a success message to the user.
-  });
-});
-
