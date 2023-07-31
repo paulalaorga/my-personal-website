c07aaf2bdf4ca2713a3fc418f761aa2d56cba492
paula <paulalaorga@gmail.com>
2023-07-31 13:38:55 +1000

    all

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-document.getElementById('openContactForm').addEventListener('click', function() {
-    const form = document.getElementById('openContactForm');
-    form.classList.toggle('hidden');
-  });
+
+              // Get references to the button and the contact form
+              const contactButton = document.getElementById('contactButton');
+              const contactForm = document.getElementById('contactForm');
+            
+              // Add an event listener to the button
+              contactButton.addEventListener('click', function() {
+                // Toggle the display of the contact form
+                if (contactForm.style.display === 'none') {
+                  contactForm.style.display = 'block';
+                } else {
+                  contactForm.style.display = 'none';
+                }
+              });
